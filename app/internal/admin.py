701a2c641ab23d1d5f6203a5edc0030557d8d03d
lyric-s <riricoplay@hotmail.com>
2025-11-28 01:39:00 +0100

    chore: TA-46 added docker files for ease of usage

--- app/internal/admin.py
+++ app/internal/admin.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from typing import Annotated
+from app.core.security import oauth2_scheme
 
 router = APIRouter(prefix="/admin", tags=["admin"])
 
-@router.get("/dashboard")
-def get_admin_dashboard():
-    return {"message": "Admin Dashboard"}
+
+@router.get("/admins/")
+def get_admin_dashboard(token: Annotated[str, Depends(oauth2_scheme)]):
+    return {"token": token}
