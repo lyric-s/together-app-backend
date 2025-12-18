a30ed53ab1ccd3700251ca7ef43b5867f1521a67
lyric-s <riricoplay@hotmail.com>
2025-12-18 18:30:44 +0100

    refactor: TA-46 code update based on coderabbit

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -5,6 +5,7 @@ def test_python_version():
     """
     Verifies we are running on the expected Python version (e.g., 3.12+).
     Currently just for workflows to pass
+    TEMPORARY !!!
     """
     assert sys.version_info.minor >= 12
     assert sys.version_info.minor < 13
